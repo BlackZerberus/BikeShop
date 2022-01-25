33d69c1988d264bc10adffe8ee5964336dd8f63c
Christopher Chappa Molina <c.e.chappa@gmail.com>
2022-01-24 22:41:58 -0300

    listas ordenadas alfabeticamente

--- monitor_api_rest/routes/api.categorias.js
+++ monitor_api_rest/routes/api.categorias.js
@@ -4,7 +4,7 @@ const db = require('../db')
 
 router.get('/', async (req, res) => {
     try {
-        const {rows} = await db.query('SELECT category_id, category_name FROM categories;')
+        const {rows} = await db.query('SELECT category_id, category_name FROM categories ORDER BY category_name ASC;')
         res.status(200).json(rows)
     } catch (error) {
         res.status(500).send(error)
--- monitor_api_rest/routes/api.marcas.js
+++ monitor_api_rest/routes/api.marcas.js
@@ -4,7 +4,7 @@ const db = require('../db')
 
 router.get('/', async (req, res) => {
     try {
-        const {rows} = await db.query('SELECT brand_id, brand_name FROM brands;')
+        const {rows} = await db.query('SELECT brand_id, brand_name FROM brands ORDER BY brand_name ASC;')
         res.status(200).json(rows)
     } catch (error) {
         res.status(500).send(error)
